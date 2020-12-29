bac5d363454a5041063b9a9229b3b4925ca5b86d
Sebastien Darche <sebastien.darche@free.fr>
2020-12-29 11:57:28 +0000

    Update motor driver range for ESC control

--- src/motor_driver.cpp
+++ src/motor_driver.cpp
@@ -17,14 +17,16 @@
 // -------------------------- Static Functions ----------------------------- //
 
 int MotorDriver::wrapCycleToPWM(double cycleDuty) {
-    if(cycleDuty > 1) cycleDuty = 1;
-    if(cycleDuty < -1) cycleDuty = -1;
+    if(cycleDuty > 1.4) cycleDuty = 1.4;
 
-    return std::floor(25.6 * cycleDuty + 76.8);
+    // No backwards propulsion
+    if(cycleDuty < 0) cycleDuty = 0.;
+
+    return std::floor((cycleDuty + 5.) * 1024 / 100);
 }
 
 double MotorDriver::wrapPWMToDouble(int pwm) {
-    return (pwm - 76.8) / 25.6;
+    return pwm / 10.24 - 5.;
 }
 
 // --------------------------    MotorDriver   ----------------------------- //
@@ -32,13 +34,17 @@ double MotorDriver::wrapPWMToDouble(int pwm) {
 MotorDriver::MotorDriver(int _pin) : pin(_pin), command(0) {
 }
 
+double MotorDriver::getCurrentOutput() {
+    return wrapPWMToDouble(command);
+} 
+
 int MotorDriver::init() {
     wiringPiSetupGpio();
     pinMode(pin, PWM_OUTPUT);
     pwmSetMode(PWM_MODE_MS);
     pwmSetRange(1024);
     pwmSetClock(375); // 50 Hz; base clock is 19,2 MHz
-    
+    pwmWrite(pin, 52);
     return 0;
 }
 
